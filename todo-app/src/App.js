1a86a5713ecd83d59cedefe30d11666a10c1fe69
김선효 <sunhyo67@gmail.com>
2024-03-21 15:45:52 +0900

    create project

---
+++ todo-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoTemplate from './components/TodoTemplate';
+
+import React from 'react';
+
+const App = () => {
+  return (
+    <TodoTemplate>
+      Todo App을 만들자!
+    </TodoTemplate>
+  )
+};
+
+export default App;
+
